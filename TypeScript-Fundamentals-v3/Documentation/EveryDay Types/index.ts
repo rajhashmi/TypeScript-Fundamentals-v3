ea85a2601259f1945dfa3f3ae0ac3faa3f0764a6
shahid <shahidhashmiraj@gmail.com>
2023-08-28 16:54:17 +0530

    documetation about📚 Optional Properties

--- TypeScript-Fundamentals-v3/Documentation/EveryDay Types/index.ts
+++ TypeScript-Fundamentals-v3/Documentation/EveryDay Types/index.ts
@@ -84,5 +84,27 @@ printCoord({ x: 3, y: 7 });
 
 // ========================= Optional Properties
 
+// Object types can also specify that some or all of their properties are optional. To do this, add a ? after the property name:
 
+function printNames(obj: { first: string; last?: string }) {
+  // ...
+}
+// Both OK
+printNames({ first: "Bob" });
+printNames({ first: "Alice", last: "Alisson" });
+// Try
+// In JavaScript, if you access a property that doesn’t exist, you’ll get the value undefined rather than a runtime error. Because of this, when you read from an optional property, you’ll have to check for undefined before using it.
+
+function printName(obj: { first: string; last?: string }) {
+  // Error - might crash if 'obj.last' wasn't provided!
+  console.log(obj.last.toUpperCase());
+// 'obj.last' is possibly 'undefined'.
+  if (obj.last !== undefined) {
+    // OK
+    console.log(obj.last.toUpperCase());
+  }
+ 
+  // A safe alternative using modern JavaScript syntax:
+  console.log(obj.last?.toUpperCase());
+}
 export{}
