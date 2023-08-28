91a2ec59e714232ce67e303442aba457a433b5c9
shahid <shahidhashmiraj@gmail.com>
2023-08-28 17:30:08 +0530

    documetation about📚 Differences Between Type Aliases and Interfaces

--- TypeScript-Fundamentals-v3/Documentation/EveryDay Types/index.ts
+++ TypeScript-Fundamentals-v3/Documentation/EveryDay Types/index.ts
@@ -197,4 +197,37 @@ printCoord({ x: 100, y: 100 });
 
 
 
+// =============================
+
+// Differences Between Type Aliases and Interfaces
+// Type aliases and interfaces are very similar, and in many cases you can choose between them freely. Almost all features of an interface are available in type, the key distinction is that a type cannot be re-opened to add new properties vs an interface which is always extendable.
+
+// Extending an interface
+
+// interface Animal {
+//   name: string;
+// }
+
+// interface Bear extends Animal {
+//   honey: boolean;
+// }
+
+// const bear = getBear();
+// bear.name;
+// bear.honey;
+        
+// Extending a type via intersections
+
+// type Animal = {
+//   name: string;
+// }
+
+// type Bear = Animal & { 
+//   honey: boolean;
+// }
+
+// const bear = getBear();
+// bear.name;
+// bear.honey;
+
 export{}
