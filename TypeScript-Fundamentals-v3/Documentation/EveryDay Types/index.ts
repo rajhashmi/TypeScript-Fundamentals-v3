64bc54ecbda992a865842eff7177f9e991900476
shahid <shahidhashmiraj@gmail.com>
2023-08-28 17:04:03 +0530

    documetation about📚 union type

--- TypeScript-Fundamentals-v3/Documentation/EveryDay Types/index.ts
+++ TypeScript-Fundamentals-v3/Documentation/EveryDay Types/index.ts
@@ -107,4 +107,71 @@ function printName(obj: { first: string; last?: string }) {
   // A safe alternative using modern JavaScript syntax:
   console.log(obj.last?.toUpperCase());
 }
+
+
+
+
+
+// ==========================================
+
+// TypeScript’s type system allows you to build new types out of existing ones using a large variety of operators. Now that we know how to write a few types, it’s time to start combining them in interesting ways.
+
+// Defining a Union Type
+// The first way to combine types you might see is a union type. A union type is a type formed from two or more other types, representing values that may be any one of those types. We refer to each of these types as the union’s members.
+
+// Let’s write a function that can operate on strings or numbers:
+
+function printId(id: number | string) {
+  console.log("Your ID is: " + id);
+}
+// OK
+printId(101);
+
+// OK
+printId("202");
+
+// Error
+printId({ myID: 22342 });
+
+// Argument of type '{ myID: number; }' is not assignable to parameter of type 'string | number'.
+// Try
+
+// Working with Union Types
+// It’s easy to provide a value matching a union type - simply provide a type matching any of the union’s members. If you have a value of a union type, how do you work with it?
+
+// TypeScript will only allow an operation if it is valid for every member of the union. For example, if you have the union string | number, you can’t use methods that are only available on string:
+
+function printid(id: number | string) {
+  console.log(id.toUpperCase());
+// Property 'toUpperCase' does not exist on type 'string | number'.
+//   Property 'toUpperCase' does not exist on type 'number'.
+}
+// Try
+// The solution is to narrow the union with code, the same as you would in JavaScript without type annotations. Narrowing occurs when TypeScript can deduce a more specific type for a value based on the structure of the code.
+
+// For example, TypeScript knows that only a string value will have a typeof value "string":
+
+function printIds(id: number | string) {
+  if (typeof id === "string") {
+    // In this branch, id is of type 'string'
+    console.log(id.toUpperCase());
+  } else {
+    // Here, id is of type 'number'
+    console.log(id);
+  }
+}
+// Try
+// Another example is to use a function like Array.isArray:
+
+function welcomePeople(x: string[] | string) {
+  if (Array.isArray(x)) {
+    // Here: 'x' is 'string[]'
+    console.log("Hello, " + x.join(" and "));
+  } else {
+    // Here: 'x' is 'string'
+    console.log("Welcome lone traveler " + x);
+  }
+}
+
+
 export{}
