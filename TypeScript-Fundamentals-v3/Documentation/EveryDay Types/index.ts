e48e71156239e692e2c6d6e4e8903e372255b4a2
shahid <shahidhashmiraj@gmail.com>
2023-08-29 00:30:28 +0530

    documetation about📚 Assertion Operator

--- TypeScript-Fundamentals-v3/Documentation/EveryDay Types/index.ts
+++ TypeScript-Fundamentals-v3/Documentation/EveryDay Types/index.ts
@@ -256,6 +256,33 @@ x = "hello";
 x = "howdy";
 // Type '"howdy"' is not assignable to type '"hello"'.
 
+// strictNullChecks off
 
+// With strictNullChecks off, values that might be null or undefined can still be accessed normally, and the values null and undefined can be assigned to a property of any type. This is similar to how languages without null checks (e.g. C#, Java) behave. The lack of checking for these values tends to be a major source of bugs; we always recommend people turn strictNullChecks on if it’s practical to do so in their codebase.
+
+
+// strictNullChecks on
+// With strictNullChecks on, when a value is null or undefined, you will need to test for those values before using methods or properties on that value. Just like checking for undefined before using an optional property, we can use narrowing to check for values that might be null:
+
+// function doSomething(x: string | null) {
+//     if (x === null) {
+//       // do nothing
+//     } else {
+//       console.log("Hello, " + x.toUpperCase());
+//     }
+//   }
+
+// ================================================== Assertion Operator
+
+// Non-null Assertion Operator (Postfix !)
+
+    // TypeScript also has a special syntax for removing null and undefined from a type without doing any explicit checking. Writing ! after any expression is effectively a type assertion that the value isn’t null or undefined:
+    
+    function liveDangerously(x?: number | null) {
+      // No error
+      console.log(x!.toFixed());
+    }
+// Try
+    // Just like other type assertions, this doesn’t change the runtime behavior of your code, so it’s important to only use ! when you know that the value can’t be null or undefined.
 
 export{}
