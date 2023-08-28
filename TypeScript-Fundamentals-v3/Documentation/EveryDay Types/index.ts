8834399f1689e37695db10cb92b61c3757c843bc
shahid <shahidhashmiraj@gmail.com>
2023-08-29 00:10:14 +0530

    documetation about📚 Type Assertions

--- TypeScript-Fundamentals-v3/Documentation/EveryDay Types/index.ts
+++ TypeScript-Fundamentals-v3/Documentation/EveryDay Types/index.ts
@@ -230,4 +230,32 @@ printCoord({ x: 100, y: 100 });
 // bear.name;
 // bear.honey;
 
+// =================================
+
+// Type Assertions
+// Sometimes you will have information about the type of a value that TypeScript can’t know about.
+
+// For example, if you’re using document.getElementById, TypeScript only knows that this will return some kind of HTMLElement, but you might know that your page will always have an HTMLCanvasElement with a given ID.
+
+// In this situation, you can use a type assertion to specify a more specific type:
+
+const myCanvass = document.getElementById("main_canvas") as HTMLCanvasElement;
+// Try
+// Like a type annotation, type assertions are removed by the compiler and won’t affect the runtime behavior of your code.
+
+// You can also use the angle-bracket syntax (except if the code is in a .tsx file), which is equivalent:
+
+const myCanvas = <HTMLCanvasElement>document.getElementById("main_canvas");
+
+// By themselves, literal types aren’t very valuable:
+
+let x: "hello" = "hello";
+// OK
+x = "hello";
+// ...
+x = "howdy";
+// Type '"howdy"' is not assignable to type '"hello"'.
+
+
+
 export{}
