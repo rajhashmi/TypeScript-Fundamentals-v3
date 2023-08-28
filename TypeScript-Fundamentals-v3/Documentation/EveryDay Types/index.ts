ed866b0311363d9279662aaf47048a5638719890
shahid <shahidhashmiraj@gmail.com>
2023-08-28 17:20:02 +0530

    documetation about📚 Type Aliases

--- TypeScript-Fundamentals-v3/Documentation/EveryDay Types/index.ts
+++ TypeScript-Fundamentals-v3/Documentation/EveryDay Types/index.ts
@@ -173,5 +173,28 @@ function welcomePeople(x: string[] | string) {
   }
 }
 
+//  ============================ Type Aliases
+
+// Type Aliases
+// We’ve been using object types and union types by writing them directly in type annotations. This is convenient, but it’s common to want to use the same type more than once and refer to it by a single name.
+
+// A type alias is exactly that - a name for any type. The syntax for a type alias is:
+
+type Point = {
+  x: number;
+  y: number;
+};
+ 
+// Exactly the same as the earlier example
+function printCoords(pt: Point) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+ 
+printCoord({ x: 100, y: 100 });
+// Try
+// You can actually use a type alias to give a name to any type at all, not just an object type. For example, a type alias can name a union type:
+
+
 
 export{}
